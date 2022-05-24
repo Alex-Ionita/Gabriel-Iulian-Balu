ef46ad743b222f03e87af851e6501930d3e8b98c
Alex-Ionita <alexandruv.ionita@gmail.com>
2022-05-24 13:44:36 +0300

    CSS part 1

---
+++ script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('.category').slick({
+        dots: true,
+        arrows: true
+    });
+    $('.testimonials').slick({
+        dots: true,
+        arrows: false
+    });
+});
